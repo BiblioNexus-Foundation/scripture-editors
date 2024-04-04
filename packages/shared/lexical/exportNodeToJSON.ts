dacbbc39e79b16b0870c08b788669a1959204113
Abel Pérez <abelper54@gmail.com>
2024-04-04 08:52:58 -0400

    Add exportNodeToJSON function to shared/lexical module and cleanup unused functions

---
+++ packages/shared/lexical/exportNodeToJSON.ts
@@ -0,0 +1,36 @@
+import { $isElementNode, LexicalNode, SerializedElementNode, SerializedLexicalNode } from "lexical";
+
+/**
+ * Exports a node to JSON.
+ * @param node - The node to export.
+ * @returns The serialized node.
+ */
+
+export function exportNodeToJSON<SerializedNode extends SerializedLexicalNode>(
+  node: LexicalNode,
+): SerializedNode {
+  const serializedNode = node.exportJSON();
+  const nodeClass = node.constructor;
+
+  if (serializedNode.type !== nodeClass.getType()) {
+    throw new Error("Serialized node type does not match node type");
+  }
+
+  if ($isElementNode(node)) {
+    const serializedChildren = (serializedNode as SerializedElementNode).children;
+    if (!Array.isArray(serializedChildren)) {
+      throw new Error("Children must be an array");
+    }
+
+    const children = node.getChildren();
+
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      const serializedChildNode = exportNodeToJSON(child);
+      serializedChildren.push(serializedChildNode);
+    }
+  }
+
+  // @ts-expect-error - This is a private method
+  return serializedNode;
+}
