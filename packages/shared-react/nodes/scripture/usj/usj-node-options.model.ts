2c36e13732240924aab4fbddd8acc1e7d43fa92e
Ira Hopkinson <irahopkinson@users.noreply.github.com>
2024-04-24 13:16:11 +1200

    improve platform editor TSDocs (#75)

--- packages/shared-react/nodes/scripture/usj/usj-node-options.model.ts
+++ packages/shared-react/nodes/scripture/usj/usj-node-options.model.ts
@@ -1,9 +1,12 @@
 import { NodeOptions } from "../../../adaptors/editor-adaptor.model";
 import { OnClick, immutableNoteCallerNodeName } from "./ImmutableNoteCallerNode";
 
+/** Options for each editor node. */
 export interface UsjNodeOptions extends NodeOptions {
   [immutableNoteCallerNodeName]: {
+    /** Possible note callers to use when caller is '+'. */
     noteCallers?: string[];
+    /** Click handler function. */
     onClick?: OnClick;
   };
 }
