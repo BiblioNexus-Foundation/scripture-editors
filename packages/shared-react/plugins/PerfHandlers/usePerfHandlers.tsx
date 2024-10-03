eb446781623987e10af4ad67da06c12871929354
Abel Pérez <abelper54@gmail.com>
2024-10-03 14:30:41 -0400

    remove console logs and fix editor style

--- packages/shared-react/plugins/PerfHandlers/usePerfHandlers.tsx
+++ packages/shared-react/plugins/PerfHandlers/usePerfHandlers.tsx
@@ -4,7 +4,6 @@ import { registerDefaultPerfHandlers } from "shared/plugins/PerfHandlers";
 
 export function usePerfHandlers(editor: LexicalEditor): void {
   return useEffect(() => {
-    console.log("registering perf handlers");
     return registerDefaultPerfHandlers(editor);
   }, [editor]);
 }
