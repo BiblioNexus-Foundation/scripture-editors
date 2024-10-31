b2238e5c3f0914e8d74cae2c40a822389b2ca99d
Abel Pérez <abelper54@gmail.com>
2024-10-31 11:20:54 -0400

    comment for deps array

--- packages/shared-react/plugins/NodesMenu/Menu/Option.tsx
+++ packages/shared-react/plugins/NodesMenu/Menu/Option.tsx
@@ -10,7 +10,7 @@ type OptionProps = {
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const MenuOption = forwardRef<HTMLButtonElement, OptionProps>(
-  ({ index, onSelect, children, onMouseEnter, ...props }, ref) => {
+  ({ index, onSelect, children, onMouseEnter, onClick, ...props }, ref) => {
     const {
       state: { selectedIndex, activeIndex },
       setActiveIndex,
@@ -26,14 +26,16 @@ export const MenuOption = forwardRef<HTMLButtonElement, OptionProps>(
         }
         setSelectedIndex(-1);
       }
-    }, [index, selectedIndex]);
+      // No need to add onSelect to the dependency array as it shouldn't change,
+      // adding it would cause possible infinite loops (possible redesign opportunity)
+    }, [index, selectedIndex, setSelectedIndex]);
 
     const handleClick = useCallback(
       (event: React.MouseEvent<HTMLButtonElement>) => {
         setSelectedIndex(index);
-        props.onClick?.(event);
+        onClick?.(event);
       },
-      [index, setSelectedIndex, props.onClick],
+      [index, setSelectedIndex, onClick],
     );
 
     const handleMouseEnter = useCallback(
