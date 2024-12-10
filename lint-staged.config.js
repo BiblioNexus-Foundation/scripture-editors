2e445e6baf05d9fbd6de864cc0321eb4e19f69d4
Ira Hopkinson <irahopkinson@users.noreply.github.com>
2024-12-10 15:09:13 +1300

    update dependencies (#216)
    - nx, lexical, husky, pnpm
    - also reduce commit time - only format since that writes and the other 2 are only checks that happen on PR

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  "**/*.{ts,tsx}": (files) => `nx affected --target=typecheck --files=${files.join(",")}`,
+  // Only run prettier since it writes to files. Type check and linting are only checks and run on
+  // each PR anyway.
+  // "**/*.{ts,tsx}": (files) => `nx affected --target=typecheck --files=${files.join(",")}`,
   "**/*.{js,ts,jsx,tsx,json,html,yml,yaml,md,cjs,mjs}": [
     (files) => `prettier --write ${files.join(" ")}`,
-    (files) => `nx affected:lint --files=${files}`,
+    // (files) => `nx affected:lint --files=${files}`,
   ],
 };
