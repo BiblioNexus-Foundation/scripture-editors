a7183271add491e4453c32dcbfee84d7cc0a2bc2
Ira Hopkinson <ira_hopkinson@sil.org>
2025-03-24 10:47:31 +1300

    platform: no leading space in ParaNode
    - extract some functions to node utils

--- packages/shared-react/plugins/ParaNodePlugin.tsx
+++ packages/shared-react/plugins/ParaNodePlugin.tsx
@@ -2,13 +2,37 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import { $getNodeByKey, LexicalEditor } from "lexical";
 import { useEffect } from "react";
 import { $isParaNode, ParaNode } from "shared/nodes/scripture/usj/ParaNode";
+import { $removeLeadingSpace, wasNodeCreated } from "../nodes/scripture/usj/node-react.utils";
+
+export default function ParaNodePlugin(): null {
+  const [editor] = useLexicalComposerContext();
+  useParaNode(editor);
+  return null;
+}
+
+function useParaNode(editor: LexicalEditor) {
+  useEffect(() => {
+    if (!editor.hasNodes([ParaNode])) {
+      throw new Error("ParaNodePlugin: ParaNode not registered on editor!");
+    }
+
+    // Update ParaNode to remove leading space and to not contain text if it is a 'b' marker.
+    return editor.registerNodeTransform(ParaNode, (node: ParaNode) =>
+      $paraNodeTransform(node, editor),
+    );
+  }, [editor]);
+}
 
 /**
- * Update ParaNode to not contain text if it is a 'b' marker (blank line). However, if the 'b'
- * ParaNode already contained text, it can be modified (backwards compatible with PT9 & USFM v3.0).
+ * Remove any leading space if the node was just created, e.g. by hitting the enter key in the
+ * middle of a paragraph. Update ParaNode to not contain text if it is a 'b' marker (blank line).
+ * However, if the 'b' ParaNode already contained text, it can be modified (backwards compatible
+ * with PT9 & USFM v3.0).
  * @param node - ParaNode thats needs updating.
  */
 function $paraNodeTransform(node: ParaNode, editor: LexicalEditor): void {
+  if (wasNodeCreated(editor, node.getKey())) $removeLeadingSpace(node.getFirstChild());
+
   if (!$isParaNode(node) || node.getMarker() !== "b" || node.isEmpty()) return;
 
   const prevEditorState = editor.getEditorState();
@@ -19,23 +43,4 @@ function $paraNodeTransform(node: ParaNode, editor: LexicalEditor): void {
   if (!wasEmpty) return;
 
   node.clear();
-}
-
-function useParaNode(editor: LexicalEditor) {
-  useEffect(() => {
-    if (!editor.hasNodes([ParaNode])) {
-      throw new Error("ParaNodePlugin: ParaNode not registered on editor!");
-    }
-
-    // Update ParaNode to not contain text if it is a b marker.
-    return editor.registerNodeTransform(ParaNode, (node: ParaNode) =>
-      $paraNodeTransform(node, editor),
-    );
-  }, [editor]);
-}
-
-export default function ParaNodePlugin(): null {
-  const [editor] = useLexicalComposerContext();
-  useParaNode(editor);
-  return null;
 }
