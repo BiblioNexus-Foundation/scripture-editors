cdee797fbb28df04149a7b1cead366a5918dc239
Ira Hopkinson <ira_hopkinson@sil.org>
2025-04-21 11:22:37 +1200

    basic apply update

---
+++ packages/platform/src/collab/delta-utils-test.data.tsx
@@ -0,0 +1,454 @@
+/** 'brethren' was pasted over the selection of 'brothers' in v2 of _initialDoc_. */
+export const replaceBrothersWithBrethren = {
+  _id: {
+    $oid: "68257f8be096d5ea4cc760cc",
+  },
+  src: "f95a8c5541d6c3748fcd245837860b5c",
+  seq: 12,
+  v: 16,
+  mv: null,
+  d: "5f500fef446b244dd557fee1:GAL:1:target",
+  m: {
+    ts: 1747287947212,
+    uId: "5f7ce00510e59f1100bc58d1",
+    source: "Editor",
+  },
+  op: {
+    ops: [
+      {
+        retain: 139,
+      },
+      {
+        attributes: {
+          segment: "verse_1_2",
+        },
+        insert: "e",
+      },
+      {
+        delete: 1,
+      },
+      {
+        retain: 2,
+      },
+      {
+        attributes: {
+          segment: "verse_1_2",
+        },
+        insert: "ren",
+      },
+      {
+        delete: 3,
+      },
+    ],
+  },
+  o: {
+    $oid: "68257f88e096d5ea4cc760cb",
+  },
+};
+
+export const initialDoc = {
+  _id: "5f500fef446b244dd557fee1:GAL:1:target",
+  ops: [
+    {
+      insert: {
+        chapter: {
+          number: "1",
+          style: "c",
+        },
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "1",
+          style: "v",
+        },
+      },
+    },
+    {
+      attributes: {
+        segment: "verse_1_1",
+      },
+      insert:
+        "Paul, an apostle—not from men nor through man, but through Jesus Christ and God the Father, who raised him from the dead— ",
+    },
+    {
+      insert: {
+        verse: {
+          number: "2",
+          style: "v",
+        },
+      },
+    },
+    {
+      attributes: {
+        segment: "verse_1_2",
+      },
+      insert: "and all the brothers who are with me, To the churches of Galatia: ",
+    },
+    {
+      insert: {
+        verse: {
+          number: "3",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_3",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "4",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_4",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "5",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_5",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "6",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_6",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "7",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_7",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "8",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_8",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "9",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_9",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "10",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_10",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "11",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_11",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "12",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_12",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "13",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_13",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "14",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_14",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "15",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_15",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "16",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_16",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "17",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_17",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "18",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_18",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "19",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_19",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "20",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_20",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "21",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_21",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "22",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_22",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "23",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_23",
+      },
+    },
+    {
+      insert: {
+        verse: {
+          number: "24",
+          style: "v",
+        },
+      },
+    },
+    {
+      insert: {
+        blank: true,
+      },
+      attributes: {
+        segment: "verse_1_24",
+      },
+    },
+    {
+      insert: "\n",
+    },
+  ],
+  _type: "http://sharejs.org/types/rich-text/v1",
+  _v: 5,
+  _m: {
+    ctime: 1723579954838,
+    mtime: 1747287819342,
+  },
+  _o: {
+    $oid: "68257f0be096d5ea4cc760be",
+  },
+};
